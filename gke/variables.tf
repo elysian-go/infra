04124ca29b544ecb1a862fbf4251a346e64a7cdc
debray_v <victor.debray@epitech.eu>
2020-04-25 20:49:54 +0200

    added documentation to variables

--- gke/variables.tf
+++ gke/variables.tf
@@ -1,16 +1,20 @@
 variable "project" {
+  description = "GCP project"
   type = string
 }
 
 variable "cluster_name" {
+  description = "GKE cluster name"
   type = string
 }
 
 variable "region" {
+  description = "GKE region"
   type = string
 }
 
 variable "location" {
+  description = "GKE subregion"
   type = string
 }
 
@@ -19,32 +23,39 @@ variable "daily_maintenance_window_start_time" {
 }
 
 variable "cluster_range_name" {
+  description = "Secondary range name in the cluster's subnetwork to use for pod IP addresses"
   type = string
 }
 
 variable "cluster_range_cidr" {
+  description = "Address range of the subnetwork, in which cluster nodes will run"
   type = string
 }
 
 variable "services_range_name" {
+  description = "Secondary range name in the cluster's subnetwork to use for pod IP addresses"
   type = string
 }
 
 variable "services_range_cidr" {
+  description = "Address range of the subnetwork, in which our k8s services will run"
   type = string
 }
 
 variable "subnet_cidr_range" {
+  description = "Address range of the subnetwork, in which our GKE cluster will run"
   type = string
 }
 
 variable "private_ip_google_access" {
+  description = "Allow VMs without external IP to access Google APIs through Private Google Access"
   type    = bool
   default = true
 }
 
 # internal IP adddresses of masters
 variable "master_ipv4_cidr_block" {
+  description = "Address range of the subnetwork for cluster's master node"
   type = string
 }
 
@@ -80,31 +91,39 @@ variable "enable_service_apis" {
 }
 
 variable "logging_service" {
+  description = "Logging service"
   type    = string
   default = "logging.googleapis.com/kubernetes"
 }
 
 variable "monitoring_service" {
+  description = "monitoring"
   type    = string
   default = "monitoring.googleapis.com/kubernetes"
 }
 
 variable "enable_private_nodes" {
+  description = "Enable private nodes on the cluster"
   type    = bool
   default = true
 }
 
 variable "enable_private_endpoint" {
+  description = "Enable private endpoint on the cluster"
   type    = bool
   default = false
 }
 
+#https://www.terraform.io/docs/providers/google/r/container_node_pool.html
 variable "node_pools" {
+  description = "Node pool configuration"
   type    = map(map(string))
   default = {}
 }
 
+#https://www.terraform.io/docs/providers/google/r/container_cluster.html#taint
 variable "node_pools_taints" {
+  description = "Node pool taints"
   type = map(list(object({ key = string, value = string, effect = string })))
   default = {
     custom-node-pool = []
@@ -112,30 +131,38 @@ variable "node_pools_taints" {
 }
 
 variable "node_pools_tags" {
+  description = "The list of instance tags applied to all nodes"
   type = map(list(string))
   default = {
     custom-node-pool = []
   }
 }
 
+#https://www.terraform.io/docs/providers/google/r/container_cluster.html#oauth_scopes-1
 variable "node_pools_oauth_scopes" {
+  description = "Set of Google API scopes to be made available on all of the node"
   type = map(list(string))
 }
 
+#AUTO_ONLY, MANUAL_ONLY
 variable "nat_ip_allocate_option" {
+  description = "External IPs allocation option"
   type = string
 }
 
 # ALL_SUBNETWORKS_ALL_IP_RANGES, ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES, LIST_OF_SUBNETWORKS
 variable "source_subnetwork_ip_ranges_to_nat" {
+  description = "How NAT should be configured per Subnetwork"
   type = string
 }
 
 # ALL_IP_RANGES, LIST_OF_SECONDARY_IP_RANGES, PRIMARY_IP_RANGE
 variable "source_ip_ranges_to_nat" {
+  description = "List of options for which source IPs in the subnetwork should have NAT enabled"
   type = list
 }
 
 variable "nat_log_filter" {
+  description = "List of options for which source IPs in the subnetwork should have NAT enabled"
   type = string
 }
